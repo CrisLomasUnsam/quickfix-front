61ce4c5c61ed4359022d671aa9a7d5517883dada
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-03-29 01:16:23 -0300

    Inicialización de proyecto

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      reporter: ['lcov', 'json', 'html', 'json-summary'],
+    },
+  }
+})
