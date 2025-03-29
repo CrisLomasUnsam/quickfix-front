b391ab819e11158964f9d1a3f749e3ed93f505fe
Cristian Conrado Villafañez Sobrecasa <40326801+KZvilla@users.noreply.github.com>
2025-03-29 14:03:20 -0300

    refactor: Update comments in protected routes to remove dummy views

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -7,13 +7,13 @@ import DummyProfessional from '../views/pages/DummyProfessional'
 export const router = createBrowserRouter([
   { path: '/', element: <Login /> },
   {
-    //Routes for the client
+    //Routes for the client, remember remove dummy view!
     element: <ProtectedRoute isClientRoute={true} />,
     children: [{ path: '/dummyClient', element: <DummyClient /> },   
     ],
   },
   {
-    //Routes for the professional
+    //Routes for the professional,remember remove dummy view!
     element: <ProtectedRoute isClientRoute={false} />,
     children: [{ path: '/dummyProfessional', element: <DummyProfessional /> }],
   },
