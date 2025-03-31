3b17533ac932e9ce471671975d58fbaa4cac7b5a
Cristian Conrado Villafañez Sobrecasa <40326801+KZvilla@users.noreply.github.com>
2025-03-31 00:46:38 -0300

    feat: Cambiar la ruta principal para que apunte a la página de inicio de sesión

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,12 +1,11 @@
 import { createBrowserRouter } from 'react-router'
-// import Login from '../views/pages/Login'
 import { ProtectedRoute } from './routes/ProtectedRoute'
 import DummyClient from '../views/pages/DummyClient'
 import DummyProfessional from '../views/pages/DummyProfessional'
-import RegisterPage from '../views/pages/RegisterPage'
+import Login from '../views/pages/Login'
 
 export const router = createBrowserRouter([
-  { path: '/', element: <RegisterPage /> },
+  { path: '/', element: <Login /> },
   {
     //Routes for the client, remember remove dummy view!
     element: <ProtectedRoute isClientRoute={true} />,
