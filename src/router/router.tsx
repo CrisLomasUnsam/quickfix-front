f1cdb2c7fd76de063623b359a69d78d9541ee74a
Valentino-afk <valentino.bortolussi.lembo@gmail.com>
2025-04-10 16:31:11 -0300

    Add path passwordRestore whit element in router.

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -9,6 +9,7 @@ import RegisterPage from '../views/pages/registerPage'
 import Frame from '../views/pages/frame'
 import DummyClient from '../views/pages/dummyClient'
 import DummyProfessional from '../views/pages/dummyProfessional'
+import PasswordRestore from '../views/pages/passwordRestore'
 
 export const router = createBrowserRouter([
   {
@@ -17,6 +18,7 @@ export const router = createBrowserRouter([
       { path: '/', element: <Login /> },
       { path: 'userSelect', element: <UserSelectionPage /> },
       { path: 'register', element: <RegisterPage /> },
+      { path: 'passwordRestore', element: <PasswordRestore /> },
     ],
   },
   {
