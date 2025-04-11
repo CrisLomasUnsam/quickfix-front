750c88f2543693ad5f5803e79a15608a0b85e729
Valentino-afk <valentino.bortolussi.lembo@gmail.com>
2025-04-11 17:55:05 -0300

    refactor userSelectPage
    Co-authored-by: Cristian Conrado Villafañez Sobrecasa <KZvilla@users.noreply.github.com>

--- src/views/pages/userSelectionPage.tsx
+++ src/views/pages/userSelectionPage.tsx
@@ -1,86 +1,53 @@
-import { useState } from "react";
-import { FormContainer } from "../components/containers/formContainer";
-import { Button, Typography, Checkbox, FormControlLabel, Box } from "@mui/material";
-import { useNavigate } from "react-router";
-
-type UserType = "customer" | "professional";
-
-const USER_TYPES: { CUSTOMER: UserType; PROFESSIONAL: UserType } = {
-    CUSTOMER: "customer",
-    PROFESSIONAL: "professional",
-};
+import { useState } from 'react'
+import { FormContainer } from '../components/containers/formContainer'
+import { Button, Typography, Checkbox, FormControlLabel } from '@mui/material'
+import { Link as MuiLink } from '@mui/material'
+import { Link as RouterLink } from 'react-router'
+import Swal from 'sweetalert2'
 
 const UserSelectionPage = () => {
-    const [selectedType, setSelectedType] = useState<UserType | null>(null);
-    const [termsAccepted, setTermsAccepted] = useState(false);
-    const navigate = useNavigate();
+  const [termsAccepted, setTermsAccepted] = useState(false)
 
-    const manageSelection = (type: UserType) => {
-        setSelectedType(type);
-        setTermsAccepted(false); // Resetea el checkbox si se cambia la opción
-    };
+  const terminos = () => {
+    Swal.fire({
+      title: 'Terminos y condiciones',
+      text: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      icon: 'info',
+    })
+  }
 
-    const manageContinue = () => {
-        if (!termsAccepted) {
-            alert("Debes aceptar los términos y condiciones para continuar.");
-            return;
+  return (
+    <FormContainer>
+      <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+        Bienvenido a QuickFix
+      </Typography>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={termsAccepted}
+            onChange={(e) => setTermsAccepted(e.target.checked)}
+          />
         }
-        console.log(`Se seleccionó y se continúa en: ${selectedType}`);
-        navigate(`/RegisterPage?type=${selectedType}`);
-    };
+        label={
+          <Typography>
+            Acepto los{' '}
+            <MuiLink onClick={terminos}>Terminos y Condiciones </MuiLink>
+          </Typography>
+        }
+      />
 
-    return (
-        <FormContainer>
-            <Typography variant="h5" align="center">Select Your Profile</Typography>
+      <Button
+        variant="contained"
+        color="success"
+        disabled={!termsAccepted}
+        component={!termsAccepted ? 'button' : RouterLink}
+        to="/register"
+        sx={{ flex: 1, minWidth: '9rem', maxWidth: '12rem' }}
+      >
+        Continuar
+      </Button>
+    </FormContainer>
+  )
+}
 
-            <Box sx={{ display: "flex", justifyContent: "center", gap: 2, marginTop: "1rem"}}>
-                <Button 
-                    variant="contained" 
-                    color="primary" 
-                    onClick={() => manageSelection(USER_TYPES.CUSTOMER)}
-                    sx={{ flex: 1, minWidth: "9rem", maxWidth: "12rem" }}
-                >
-                    Customer
-                </Button>
-                <Button 
-                    variant="contained" 
-                    color="secondary" 
-                    onClick={() => manageSelection(USER_TYPES.PROFESSIONAL)}
-                    sx={{ flex: 1, minWidth: "9rem", maxWidth: "12rem" }}
-                >
-                    Professional
-                </Button>
-            </Box>
-           
-            {selectedType && (
-                <>
-                    <Box sx={{ maxHeight: "10rem", overflow: "auto", padding: "1rem", border: "1px solid #ccc", borderRadius: "10%" }}>
-                        <Typography variant="body1" sx={{ textAlign: "center", marginTop: "1rem" }}>
-                            {selectedType === USER_TYPES.CUSTOMER
-                                ? "Términos y condiciones del Cliente...."
-                                : "Términos y condiciones del Profesional"
-                            }
-                        </Typography>
-                    </Box>
-
-                    <FormControlLabel
-                        control={<Checkbox checked={termsAccepted} onChange={(e) => setTermsAccepted(e.target.checked)} />}
-                        label="Acepto los términos y condiciones"
-                    />
-
-                    <Button 
-                        variant="contained" 
-                        color="success" 
-                        onClick={manageContinue}
-                        sx={{ flex: 1, minWidth: "9rem", maxWidth: "12rem" }}
-                    >
-                        Continue
-                    </Button>
-                </>
-            )}
-        </FormContainer>
-    );
-};
-
-export default UserSelectionPage;
-
+export default UserSelectionPage
